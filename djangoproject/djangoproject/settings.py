bc486dd71d1f1a3afdefd69bb87eecf61e8a6569
xbobekf <filip.bobek13@gmail.com>
2020-10-06 17:42:50 +0200

    nginx serve staticfiles

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from djangoproject.keyconfig import Database, Secrets
@@ -123,4 +123,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
