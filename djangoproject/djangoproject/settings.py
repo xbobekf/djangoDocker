b73ae762f7cffbd7f9515a324271974a9b5c3a2e
xbobekf <filip.bobek13@gmail.com>
2020-10-06 12:47:59 +0200

    allow host

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = Secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "192.168.0.221"]
 
 
 # Application definition
